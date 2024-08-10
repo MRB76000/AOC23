eefd173f061f97bc58c3b6be4a51b13e65c60ad1
MRB76000 <mb840705@gmail.com>
2024-08-10 19:21:18 -0400

    first commit

---
+++ day1/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    long fin = 0;
+    vector<int> myVector;
+
+    string stronk;
+
+    ifstream file("text.txt");
+
+    while (getline(file, stronk))
+    {
+        for (char i : stronk)
+        {
+            if (isdigit(i))
+            {
+                myVector.push_back(i);
+            }
+        }
+        char first = myVector[0];
+        
+        char last = myVector[myVector.size() - 1];
+
+        
+        
+        int lex = ((first - 48) * 10) + (last -48);
+
+        // cout << lex;
+
+        
+
+        // cout << '\n';
+
+
+        myVector.clear();
+         
+        fin += lex;
+        
+        
+    }
+
+    cout << fin;
+
+    file.close();
+
+    return 0;
+}
